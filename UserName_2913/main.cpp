c070abd2166e4064e6a75f34a3ebe8d30f51efa3
Austin Keil <austin.c.keil@gmail.com>
2018-10-01 21:06:08 -0700

    submission successful (250 points)

--- UserName_2913/main.cpp
+++ UserName_2913/main.cpp
@@ -1,6 +1,33 @@
 #include <iostream>
+#include <string>
+#include <algorithm>
+#include <array>
 
-int main() {
-    std::cout << "Hello, World!" << std::endl;
-    return 0;
-}
+using namespace std;
+
+class UserName
+{
+public:
+    UserName() {}
+    string newMember(vector<string> existingNames, string newName)
+    {
+        if(find(existingNames.begin(), existingNames.end(), newName) == existingNames.end())
+        {
+            return newName;
+        }
+
+        string curName = newName;
+        int appendage = 1;
+        while(find(existingNames.begin(), existingNames.end(), curName + to_string(appendage)) != existingNames.end()){
+            appendage = appendage + 1;
+        }
+        return curName + to_string(appendage);
+    }
+};
+
+//int main(){
+//    UserName newUserName;
+//    vector<string> existingNames = {"Joe", "Joe1", "Tom", "Joe2", "Nicole"};
+//    cout << newUserName.newMember(existingNames, "Joe");
+//}
+
